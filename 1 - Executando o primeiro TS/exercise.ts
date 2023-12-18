6015513ee723e4217e12ee04c917a4aaf39c9aff
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-18 17:24:58 -0300

    feat: section exercise

---
+++ 1 - Executando o primeiro TS/exercise.ts
@@ -0,0 +1,10 @@
+// Criar uma função que aceita dois argumentos, somente do tipo númerico e exiba a soma entre os dois.
+
+const a = 10;
+const b = 5;
+
+function sum(a: number, b: number) {
+  return a + b;
+}
+
+console.log(sum(a, b));
