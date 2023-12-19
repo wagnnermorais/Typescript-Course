0b5553181dc2ca5ce9a8c28ff413632af561ec91
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-18 21:56:41 -0300

    feat: solving section exercise

---
+++ 2 - Fundamentos do Typescript/exercise.ts
@@ -0,0 +1,18 @@
+// section exercise
+// Crie uma variável que recebe um número, converta este número para uma string em uma nova variável
+// Está variável de conversão deve estar tipada por inferência.
+// Imprima este número em uma string maior, utilizando template strings.
+
+let value: number | string = 10;
+
+value = "10";
+
+console.log(`O número selecionado é: ${value}`);
+
+// solução do instrutor:
+
+// const n1: number = 10
+// const numberToString = n1.toString()
+// const print: string = `Imprima o número ${numberToString}`
+
+// console.log(print)
