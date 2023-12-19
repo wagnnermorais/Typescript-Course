74c63344b449953a9c8aabadb35b7d9aead473e3
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-18 21:57:17 -0300

    feat: adding array types

---
+++ 3 - Avançando em Tipos/3.1 - Arrays/3.1.1 - Outra Sintaxe de Arrays/index.ts
@@ -0,0 +1,6 @@
+const nums: Array<number> = [100, 200, 300];
+
+nums.push(400);
+
+console.log(nums);
+console.log(nums[2]);
---
+++ 3 - Avançando em Tipos/3.1 - Arrays/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let numbers = [1, 2, 3];
+numbers.push(5, 4, 2);
+console.log(numbers);
+const nomes = ["Wagner", "Kiwi", "Márcio"];
+nomes.push("Filipe");
+console.log(nomes);
