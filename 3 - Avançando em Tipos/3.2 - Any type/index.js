5fc564835fbf12c81b1783a1141705ce2aab0023
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-18 21:57:27 -0300

    feat: adding any type

---
+++ 3 - Avançando em Tipos/3.2 - Any type/index.js
@@ -0,0 +1,4 @@
+"use strict";
+const arr = [1, 2, "Teste", [], { nome: "Wagner" }];
+console.log(arr);
+arr.push(10, "Kiwi", true);
---
+++ 3 - Avançando em Tipos/3.2 - Any type/index.ts
@@ -0,0 +1,5 @@
+const arr: any = [1, 2, "Teste", [], { nome: "Wagner" }];
+
+console.log(arr);
+
+arr.push(10, "Kiwi", true);
