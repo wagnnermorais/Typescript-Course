9fbbdad7c0c09800da561d863ae9410d1f0b4ba2
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-19 12:07:35 -0300

    feat: creating variables to use string types

--- 2 - Fundamentos do Typescript/2.2 - Strings/index.ts
+++ 2 - Fundamentos do Typescript/2.2 - Strings/index.ts
@@ -5,7 +5,6 @@ console.log(firstName.toUpperCase());
 console.log(firstName.toLocaleLowerCase());
 
 // iniciando variável sem valor, porém, com tipo.
-
 let fullName: string;
 
 const lastName: string = "Morais";
@@ -13,3 +12,7 @@ const lastName: string = "Morais";
 fullName = `${firstName} ${lastName}`;
 
 console.log(fullName);
+
+const char = "A";
+
+console.log(char);
