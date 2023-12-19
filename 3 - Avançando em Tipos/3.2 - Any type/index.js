2d9bfe2ca4d6c6fd832c60da63f30eee509283d4
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-19 12:20:49 -0300

    feat: creating variables to use any type

--- 3 - Avançando em Tipos/3.2 - Any type/index.js
+++ 3 - Avançando em Tipos/3.2 - Any type/index.js
@@ -2,3 +2,5 @@
 const arr = [1, 2, "Teste", [], { nome: "Wagner" }];
 console.log(arr);
 arr.push(10, "Kiwi", true);
+arr.push([...arr, 1, 2, 3], ["Green", "Yellow", "Red"]);
+console.log(arr);
--- 3 - Avançando em Tipos/3.2 - Any type/index.ts
+++ 3 - Avançando em Tipos/3.2 - Any type/index.ts
@@ -3,3 +3,6 @@ const arr: any = [1, 2, "Teste", [], { nome: "Wagner" }];
 console.log(arr);
 
 arr.push(10, "Kiwi", true);
+arr.push([...arr, 1, 2, 3], ["Green", "Yellow", "Red"]);
+
+console.log(arr);
