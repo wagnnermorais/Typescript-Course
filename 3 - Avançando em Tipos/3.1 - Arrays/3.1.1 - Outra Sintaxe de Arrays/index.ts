af7c5958a7cdaab14ba7e8d7c458f9b7b7c53c78
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-19 12:20:31 -0300

    feat: creating variables to use array types

--- 3 - Avançando em Tipos/3.1 - Arrays/3.1.1 - Outra Sintaxe de Arrays/index.ts
+++ 3 - Avançando em Tipos/3.1 - Arrays/3.1.1 - Outra Sintaxe de Arrays/index.ts
@@ -1,6 +1,7 @@
 const nums: Array<number> = [100, 200, 300];
 
 nums.push(400);
+nums.push(-25, 3.3);
 
 console.log(nums);
 console.log(nums[2]);
--- 3 - Avançando em Tipos/3.1 - Arrays/index.ts
+++ 3 - Avançando em Tipos/3.1 - Arrays/index.ts
@@ -1,11 +1,14 @@
 let numbers: number[] = [1, 2, 3];
 
 numbers.push(5, 4, 2);
+numbers.push(3.2, 5.5555, -13);
 
 console.log(numbers);
 
 const nomes: string[] = ["Wagner", "Kiwi", "Márcio"];
 
 nomes.push("Filipe");
+nomes.push("A");
+nomes.push("Daniel");
 
 console.log(nomes);
