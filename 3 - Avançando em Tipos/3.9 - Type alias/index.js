1e32f7aced9039f33c7aeeaee7b96125d4fd238d
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-19 22:42:03 -0300

    feat: creating an alias to type a parameter

---
+++ 3 - Avançando em Tipos/3.9 - Type alias/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function showId(id) {
+    console.log(`O ID é: ${id}`);
+}
+showId(1);
+showId("85");
---
+++ 3 - Avançando em Tipos/3.9 - Type alias/index.ts
@@ -0,0 +1,8 @@
+type ID = string | number;
+
+function showId(id: ID) {
+  console.log(`O ID é: ${id}`);
+}
+
+showId(1);
+showId("85");
