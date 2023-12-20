b7db5ba057888699d99f46ab60a6e317de43e8c7
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-19 22:43:06 -0300

    feat: using non null operator

---
+++ 3 - Avançando em Tipos/3.13 - Non-null Assertion Operator/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// fazendo o get de algum suposto elemento DOM
+const value = document.getElementById("element");
+// dessa forma, o typescript acusa o erro que o valor é possivelmente "Nulo"
+// console.log(value.innerText)
+// a solução é usando o non-null operator
+console.log(value.innerText);
