57d8f0a24c2ba3d81242b078192e56d1e88b0cfa
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-19 22:42:54 -0300

    feat: typing parameter values with literal types

---
+++ 3 - Avançando em Tipos/3.12 - Literal Types/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function showDirection(direction) {
+    console.log(`A direção é: ${direction}`);
+}
+showDirection("center");
+showDirection("left");
+showDirection("right");
---
+++ 3 - Avançando em Tipos/3.12 - Literal Types/index.ts
@@ -0,0 +1,7 @@
+function showDirection(direction: "left" | "right" | "center") {
+  console.log(`A direção é: ${direction}`);
+}
+
+showDirection("center");
+showDirection("left");
+showDirection("right");
