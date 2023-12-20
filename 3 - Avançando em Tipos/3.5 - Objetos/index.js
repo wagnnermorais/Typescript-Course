7bf4109c3543080cf5f4241761735b51e3403ed4
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-19 22:40:00 -0300

    feat: adding types to objects

---
+++ 3 - Avançando em Tipos/3.5 - Objetos/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function passCoordinates(coordinates) {
+    console.log(`X coordinates: ${coordinates.x}`);
+    console.log(`Y coordinates: ${coordinates.y}`);
+}
+passCoordinates({ x: -34.2678, y: 150.8764 });
---
+++ 3 - Avançando em Tipos/3.5 - Objetos/index.ts
@@ -0,0 +1,6 @@
+function passCoordinates(coordinates: { x: number; y: number }) {
+  console.log(`X coordinates: ${coordinates.x}`);
+  console.log(`Y coordinates: ${coordinates.y}`);
+}
+
+passCoordinates({ x: -34.2678, y: 150.8764 });
