dd0d5e2dbeb9ae33fac98714f5a44a11546aacef
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-12-30 00:53:58 -0300

    feat: using * to import all data

---
+++ 9 - Typescript modules/9.5 - Importando tudo/index.ts
@@ -0,0 +1,7 @@
+import * as numbers from "./numbers";
+
+console.log(numbers);
+
+const number = numbers.n1;
+
+console.log(number);
---
+++ 9 - Typescript modules/9.5 - Importando tudo/numbers.ts
@@ -0,0 +1,3 @@
+export const n1: number = 10;
+export const n2: number = 20;
+export const n3: number = 30;
