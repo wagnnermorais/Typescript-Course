9cdf335b4115d70488e6300f1ce96f2d4c3078f9
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-01-02 22:01:12 -0300

    feat: creating task component

---
+++ 12 - (Project) React Todo List/src/components/Tasks.tsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+import styles from "../styles/Tasks.module.css";
+// type Props = {}
+
+const Tasks = () => {
+  return (
+    <div className={styles.container}>
+      <h3 className={styles.title}>Suas tarefas:</h3>
+      <p>tasks...</p>
+    </div>
+  );
+};
+
+export default Tasks;
