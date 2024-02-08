7402f6e96aec8fc61224d1be00bc0095a15a3a0b
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-02-08 12:12:09 -0300

    feat: importing database files

--- 14 - (Project) API Restful Com Typescript/src/app.ts
+++ 14 - (Project) API Restful Com Typescript/src/app.ts
@@ -1,1 +1,23 @@
-console.log("Worked.")
+require("dotenv").config()
+
+import express from "express"
+import config from "config"
+import router from "./router"
+import database from "../config/database"
+import Logger from "../config/looger"
+
+const app = express()
+
+// JSON Middleware
+app.use(express.json())
+
+// Routes
+app.use("/api/", router)
+
+// Porta da aplicação
+const port = config.get<number>("port")
+app.listen(port, async () => {
+  await database()
+  Logger.info(`Aplicação está funcionando na porta: ${port}`)
+})
+
