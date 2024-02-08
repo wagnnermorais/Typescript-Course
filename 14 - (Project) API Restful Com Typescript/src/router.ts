0008d2dfa8643bbdf69993c3c7726ecb8a748b3a
wagnnermorais <wagnermoraiscnt@gmail.com>
2024-02-08 19:14:56 -0300

    feat: adding post request to router

--- 14 - (Project) API Restful Com Typescript/src/router.ts
+++ 14 - (Project) API Restful Com Typescript/src/router.ts
@@ -1,7 +1,12 @@
 import { Router, Request, Response } from "express"
+import { createMovie } from "./controllers/movieController"
+import { validate } from "./middleware/handleValidation"
+import { movieCreateValidation } from "./middleware/movieValidation"
 
 const router = Router()
 
-export default router.get("/test", (req: Request, res: Response) => {
-  res.status(200).send("API Working!!")
-})
+export default router
+  .get("/test", (req: Request, res: Response) => {
+    res.status(200).send("API Working!!")
+  })
+  .post("/movie", movieCreateValidation(), validate, createMovie)
